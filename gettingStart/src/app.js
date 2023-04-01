78c92bfd5639b88e6d3b76f11b7bc6d4f0f4fef3
Vipul Kumar <92398016+itsvipulkumar@users.noreply.github.com>
2023-04-02 01:28:01 +0530

    Update app.js

--- gettingStart/src/app.js
+++ gettingStart/src/app.js
@@ -63,18 +63,24 @@ const collection = new mongoose.model("Developer", playlistSchema);
 const createDOC = async () => {
     try {
         const doc = new collection({
-            name: "Sumit",
-            rn: 56,
-            city: "Dearadun",
+            name: "Mohit",
+            rn: 14,
+            city: "Dehradun",
 
         })
         const doc2 = new collection({
-            name: "Vipul",
-            rn: 61,
-            city: "Bijnor",
+            name: "Poonam",
+            rn: 40,
+            city: "Delhi",
 
         })
-        const result = await collection.insertMany([doc, doc2]);
+        const doc3 = new collection({
+            name: "madha",
+            rn: 23,
+            city: "Delhi",
+
+        })
+        const result = await collection.insertMany([doc, doc2, doc3]);
         // console.log(result)
     }
     catch (err) {
@@ -91,11 +97,87 @@ const createDOC = async () => {
 
 // getting the data from the database or collections
 
+// const getDocument = async () => {
+//     const res = await collection.find({ rn: 61 });
+//     console.log(res)
+// }
+
+// getDocument();
+
+
+
+
+
+
+
+
+
+
+// getting value by operators
+
+
+// COMPARISION OPERATOR
+/**
+ * 
+Name
+Description
+$eq
+Matches values that are equal to a specified value.
+$gt
+Matches values that are greater than a specified value.
+$gte
+Matches values that are greater than or equal to a specified value.
+$in
+Matches any of the values specified in an array.
+$lt
+Matches values that are less than a specified value.
+$lte
+Matches values that are less than or equal to a specified value.
+$ne
+Matches all values that are not equal to a specified value.
+$nin
+Matches none of the values specified in an array.
+ * 
+ */
+
+// const getDocument = async () => {
+//     const res = await collection.find({ rn: { $lt: 10 } }); // data will show those haveless then 10
+//     console.log(res)
+// }
+
+// const getDocument = async () => {
+//     const res = await collection.find({ rn: { $gt: 50 } }); // data will show those have greater then 10
+//     console.log(res)
+// }
+
+
+
+// const getDocument = async () => {
+//     const res = await collection.find({ city: { $in: ["Delhi", "Dehradun"] } }); // data will show those belod to "Delhi", "Dehradun" city
+
+//     console.log(res)
+// }
+
+
+// LOGOCAL OPERATOR
+/**
+ * and , or nor xor
+ */
+
+// const getDocument = async () => {
+//     const res = await collection
+//         .find({ $or: [{ city: "Delhi" }, { rn: { $gt: 50 } }] });
+//        // will show the data which have either city : delhi ya phir roll number grater than 50
+//     console.log(res)
+// }
+
+
 const getDocument = async () => {
-    const res = await collection.find({ name: "Vipul" }, { name: 1 }, { _id: 0 });
+    const res = await collection
+        .find({ $and: [{ city: "Delhi" }, { rn: { $gt: 50 } }] });
+    // will show the data which only have  city : delhi and roll number grater than 50
     console.log(res)
 }
-
 getDocument();
 
 
